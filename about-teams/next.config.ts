57bf858f71b4890f3eb6b7336bc2969e8153bf32
doconghieu <doconghieu4198@gmail.com>
2026-01-03 22:14:07 +0700

    [CHORE] config build src

--- about-teams/next.config.ts
+++ about-teams/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/about-teams",
+  assetPrefix: "/about-teams/",
 };
 
 export default nextConfig;
