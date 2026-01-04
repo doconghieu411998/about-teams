3ecb926bea7979ff0880cbea672bb9f6e307fd6e
doconghieu <doconghieu4198@gmail.com>
2026-01-04 10:47:16 +0700

    Updates

--- _next/static/26auWlI0xFmlVb-2at1Ko/_buildManifest.js
+++ _next/static/26auWlI0xFmlVb-2at1Ko/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/about-teams//_next/:path+",
+        "destination": "/about-teams/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
